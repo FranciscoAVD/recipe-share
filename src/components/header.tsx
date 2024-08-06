e1bcbeda9921e87fe1e297a0c44f98bf2f4197d7
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-08-05 21:57:22 -0400

    updated styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 export default function Header() {
   return (
     <header className="absolute top-0 z-50 w-full pt-4">
-      <div className=" container grid grid-cols-3 items-center text-white text-sm text-center rounded-full">
+      <div className=" container grid grid-cols-3 items-start text-white text-sm text-center rounded-full">
         <nav className="w-fit bg-neutral-900 py-2 px-6 rounded-full">
           <ul className="flex gap-x-8  uppercase">
             <li>
@@ -29,9 +29,9 @@ export default function Header() {
         <Link href="/" className="text-2xl font-semibold">
           <Image src={logo} alt="logo" className="size-28 mx-auto rounded-full"/>
         </Link>
-        <div className="rounded-full space-x-4 uppercase">
-          <Button asChild className="bg-yellow-500"><Link href="/">Sign up</Link></Button>
-          <Button asChild className=""><Link href="/">Sign in</Link></Button>
+        <div className="w-fit ml-auto rounded-full uppercase">
+          <Link href="/" className="bg-neutral-900 hover:bg-yellow-500 py-2 pl-6 pr-3 text-yellow-500 hover:text-white rounded-l-full transition">Sign up</Link>
+          <Link href="/" className="bg-neutral-900 hover:bg-lime-500 py-2 pr-6 pl-3 text-lime-500 hover:text-white rounded-r-full transition">Sign in</Link>
         </div>
       </div>
     </header>
