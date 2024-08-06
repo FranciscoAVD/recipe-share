cfb066ac65e330ed652503ce910a2b6ad05a0fe2
Francisco Victoriano <fvictoriano.dev@gmail.com>
2024-08-05 22:10:05 -0400

    updated styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../../public/logo.jpg"
+import logo from "../../public/logo.jpg";
 import { Button } from "./ui/button";
 
 export default function Header() {
@@ -8,7 +8,7 @@ export default function Header() {
     <header className="absolute top-0 z-50 w-full pt-4">
       <div className=" container grid grid-cols-3 items-start text-white text-sm text-center rounded-full">
         <nav className="w-fit bg-neutral-900 py-2 px-6 rounded-full">
-          <ul className="flex gap-x-8  uppercase">
+          <ul className="flex gap-x-8 uppercase">
             <li>
               <Link href="/" className="hover:text-yellow-500 transition">
                 Home
@@ -27,11 +27,25 @@ export default function Header() {
           </ul>
         </nav>
         <Link href="/" className="text-2xl font-semibold">
-          <Image src={logo} alt="logo" className="size-28 mx-auto rounded-full"/>
+          <Image
+            src={logo}
+            alt="logo"
+            className="size-28 mx-auto rounded-full"
+          />
         </Link>
         <div className="w-fit ml-auto rounded-full uppercase">
-          <Link href="/" className="bg-neutral-900 hover:bg-yellow-500 py-2 pl-6 pr-3 text-yellow-500 hover:text-white rounded-l-full transition">Sign up</Link>
-          <Link href="/" className="bg-neutral-900 hover:bg-lime-500 py-2 pr-6 pl-3 text-lime-500 hover:text-white rounded-r-full transition">Sign in</Link>
+          <Link
+            href="/"
+            className="bg-neutral-900 hover:bg-yellow-500 py-2 pl-6 pr-3 text-yellow-500 hover:text-white rounded-l-full transition"
+          >
+            Sign up
+          </Link>
+          <Link
+            href="/"
+            className="bg-neutral-900 hover:bg-lime-600 py-2 pr-6 pl-3 text-lime-600 hover:text-white rounded-r-full transition"
+          >
+            Sign in
+          </Link>
         </div>
       </div>
     </header>
